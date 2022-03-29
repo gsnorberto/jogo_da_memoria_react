732499c53466f0c82e1f4481f876bee0b3bfa4b9
Gabriel Norberto <sn_gabriel@outlook.com>
2022-03-29 10:36:48 -0300

    Em desenvolvimento...

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,53 @@
 import * as C from "./App.styles";
 import logoImage from './assets/devmemory_logo.png'
+import { InfoItem } from "./components/InfoItem";
+import { Button } from "./components/Button";
+import RestartIcon from "./svgs/restart.svg"
+import { useEffect, useState } from "react";
+import { GridItemType } from "./types/GridItemType";
+import { items } from "./data/items";
 
 const App = () => {
+   const [playing, setPlaying] = useState<boolean>(false);
+   const [timeElapsed, setTimeElapsed] = useState<number>(0); // Timer do Jogo
+   const [moveCount, setMoveCount] = useState<number>(0); // Qnt. de movimentos feitos
+   const [shownCount, setShownCount] = useState<number>(0); // Qnt. de cartas sendo exibidas.
+   const [gridItems, setGridItems] = useState<GridItemType[]>([]);
+
+   useEffect(() => {
+      resetAndCreateGrid();
+   }, []);
+
+   const resetAndCreateGrid = () => {
+      //reset game
+      setTimeElapsed(0);
+      setMoveCount(0);
+      setShownCount(0);
+
+      //create game
+      let tmpGrid: GridItemType[] = [];
+      for(let i = 0; i < (items.length * 2); i++){
+         tmpGrid.push({
+            item: null,
+            shown: false,
+            permanentShown: false
+         })
+      }
+
+      for(let w = 0; w < 2; w++){
+         for(let i = 0; i < items.length; i++){
+            let pos = Math.floor(Math.random() * (items.length * 2));
+            tmpGrid[pos].item = i;
+         }
+      }
+
+
+      setGridItems(tmpGrid);
+
+      //play game
+      setPlaying(true);
+   }
+
    return (
       <C.container>
          <C.Info>
@@ -10,13 +56,17 @@ const App = () => {
             </C.LogoLink>
 
             <C.InfoArea>
-               ...
+               <InfoItem label="Tempo" value="00:00" />
+               <InfoItem label="Movimentos" value="0" />
             </C.InfoArea>
 
-            <button>Reiniciar</button>
+            <Button label="Reiniciar" icon={RestartIcon} onClick={resetAndCreateGrid}/>
          </C.Info>
-         <C.GridArea>
 
+         <C.GridArea>
+            <C.Grid>
+
+            </C.Grid>
          </C.GridArea>
       </C.container>
    );
